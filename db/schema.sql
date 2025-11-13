176ffdc694c6d1cc75d9c7fe3dc01481b0575f26
Sherin <sherin.daniel89@yahoo.com>
2025-11-12 21:36:08 -0500

    Schema done

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,33 @@
 -- TODO
+DROP TABLE orders_products CASCADE;
+DROP TABLE orders CASCADE;
+DROP TABLE products CASCADE;
+DROP TABLE users CASCADE;
+
+ -- create tables -- order matters -- 
+ CREATE TABLE users(
+    id SERIAL PRIMARY KEY, 
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+ );
+
+ CREATE TABLE orders(
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    note TEXT,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
+ );
+
+ CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price numeric(12,2) NOT NULL
+ );
+
+ CREATE TABLE orders_products(
+    order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_id, product_id)
+ );
