3720555001b7876b0ba05a697544488afde4d699
Sherin <sherin.daniel89@yahoo.com>
2025-11-12 23:21:15 -0500

    GET // Order updated

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -13,3 +13,33 @@ export async function createOrder(date, note, userID) {
     [date, note, userId]
   );
 }
+// get all orders for a user
+export async function getOrdersByUserId(userId) {
+  const { rows } = await db.query(
+    `
+      SELECT *
+      FROM orders
+      WHERE user_id = $1
+      ORDER BY id;
+    `,
+    [userId]
+  );
+
+  return rows;
+}
+
+// get one order by id
+export async function getOrderById(id) {
+  const {
+    rows: [order],
+  } = await db.query(
+    `
+      SELECT *
+      FROM orders
+      WHERE id = $1;
+    `,
+    [id]
+  );
+
+  return order ?? null;
+}
