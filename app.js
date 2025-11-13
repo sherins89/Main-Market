779950871cd541f2f1de2d89cd52f3f8bb8c1cea
Sherin <sherin.daniel89@yahoo.com>
2025-11-12 21:58:03 -0500

    app.js updated for User token

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import express from "express";
+import getUserFromToken from "./middleware/getUserFromToken.js";
+
 const app = express();
+
+app.use(express.json());
+
+// if a token is sent, put the user on req.user
+app.use(getUserFromToken);
+
 export default app;
